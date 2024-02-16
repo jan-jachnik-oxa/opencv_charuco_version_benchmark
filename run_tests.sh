1026e1b24a604987a034f9fefe16b3a0178a2c70
Jan Jachnik <jan.jachnik@oxa.tech>
2024-02-16 15:50:41 +0000

    Added more versions to test

--- run_tests.sh
+++ run_tests.sh
@@ -7,6 +7,20 @@ deactivate
 
 echo ""
 
+echo "Running tests on 4.6.0"
+source venv_4.6.0/bin/activate
+python test_charuco.py --old-api
+deactivate
+
+echo ""
+
+echo "Running tests on 4.8.1"
+source venv_4.8.1/bin/activate
+python test_charuco.py
+deactivate
+
+echo ""
+
 echo "Running tests on 4.9.0"
 source venv_4.9.0/bin/activate
 python test_charuco.py
--- setup_venvs.sh
+++ setup_venvs.sh
@@ -5,6 +5,16 @@ source venv_4.5.5/bin/activate
 pip install opencv-contrib-python==4.5.5.62
 deactivate
 
+virtualenv venv_4.6.0
+source venv_4.6.0/bin/activate
+pip install opencv-contrib-python==4.6.0.66
+deactivate
+
+virtualenv venv_4.8.1
+source venv_4.8.1/bin/activate
+pip install opencv-python==4.8.1.78
+deactivate
+
 virtualenv venv_4.9.0
 source venv_4.9.0/bin/activate
 pip install opencv-python==4.9.0.80
