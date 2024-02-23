ad9d33d734eb5d1227539668b2e346aaa624073f
Jan Jachnik <jan.jachnik@oxa.tech>
2024-02-23 11:30:45 +0000

    Changed to use headless opencv to enlble testing on other machines

--- setup_venvs.sh
+++ setup_venvs.sh
@@ -2,20 +2,20 @@
 
 virtualenv venv_4.5.5
 source venv_4.5.5/bin/activate
-pip install opencv-contrib-python==4.5.5.62
+pip install opencv-contrib-python-headless==4.5.5.62
 deactivate
 
 virtualenv venv_4.6.0
 source venv_4.6.0/bin/activate
-pip install opencv-contrib-python==4.6.0.66
+pip install opencv-contrib-python-headless==4.6.0.66
 deactivate
 
 virtualenv venv_4.8.1
 source venv_4.8.1/bin/activate
-pip install opencv-python==4.8.1.78
+pip install opencv-python-headless==4.8.1.78
 deactivate
 
 virtualenv venv_4.9.0
 source venv_4.9.0/bin/activate
-pip install opencv-python==4.9.0.80
+pip install opencv-python-headless==4.9.0.80
 deactivate
